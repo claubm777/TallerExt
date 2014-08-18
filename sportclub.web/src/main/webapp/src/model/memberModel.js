289113ad60dc995a1846b87e0aeb538408b546f8
claubm777 <claubm777@hotmail.com>
2014-08-18 02:26:49 -0500

    Commit inicial

--- sportclub.web/src/main/webapp/src/model/memberModel.js
+++ sportclub.web/src/main/webapp/src/model/memberModel.js
@@ -56,6 +56,29 @@ define(['model/_memberModel'], function() {
             else if(!attrs.partnerId){
                 validationMessage = "The partner Id can't be empty";
             }
+            else {
+                var dia1=parseInt(attrs.birthDate.substring(0,2));
+                var mes1=parseInt(attrs.birthDate.substring(3,5))-1;
+                var anho1=parseInt(attrs.birthDate.substring(6,10));
+                var fechaActual=new Date();
+                var dia2=fechaActual.getDate();
+                var mes2=fechaActual.getMonth();
+                var anho2=fechaActual.getFullYear();
+                //alert(dia1+";"+mes1+";"+anho1);
+                //alert(dia2+";"+mes2+";"+anho2);
+                if(anho1<anho2-100||(anho1==anho2-100 && mes1<mes2)||(anho1==anho2-100 && mes1==mes2 && dia1<dia2)){
+                   validationMessage = "La persona no puede tener mas de 100 anhos";
+                }
+                else if(attrs.docNumber.length<5){
+                   validationMessage = "El número de documento debe tener por lo menos 5 caracteres";
+                }
+                else if(anho1>anho2||(anho1==anho2 && mes1>mes2)||(anho1==anho2 && mes1==mes2 && dia1>=dia2)){
+                   validationMessage = "La persona no puede haber nacido en una fecha igual o posterior a la actual";
+                }
+            }
+
+
+            
             if(validationMessage.length>0){
                return validationMessage;
             }
